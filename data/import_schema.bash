67aabb895478a6bf94c29eb6f3b5e99adf3dde9c
kzhirata <kzhirata77@gmail.com>
2018-07-21 15:49:29 +0900

    add suggest_schema.json

--- data/import_schema.bash
+++ data/import_schema.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl -X POST -H 'Content-type: application/json' --data-binary @company_schema.json http://localhost:8983/solr/company/schema
+SOLR_CORE_NAME="company"
+
+curl -X POST -H 'Content-type: application/json' --data-binary @suggest_schema.json http://localhost:8983/solr/${SOLR_CORE_NAME}/schema
+
+curl -X POST -H 'Content-type: application/json' --data-binary @company_schema.json http://localhost:8983/solr/${SOLR_CORE_NAME}/schema
