8e39b2ec9168c094d00bfd96b0a4d09cadc47eda
kzhirata <kzhirata77@gmail.com>
2018-07-21 12:55:47 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-PROJECT="company"
+
+APP_ROOT=`pwd`
+PROJECT="proj"
+SOLR_CORE_NAME="company"
 
 mkdir ./container/${PROJECT}
 
-docker run --name solr -d -p 8983:8983 -v /Users/kzhirata/tmp/solr/container:/opt/solr/server/solr/${PROJECT} -t solr
+docker run --name solr -d -p 8983:8983 -v ${APP_ROOT}/container/${PROJECT}:/opt/solr/server/solr/${PROJECT} -t solr
+
+docker exec -it solr bash -c "/opt/solr/bin/solr create -c ${SOLR_CORE_NAME}"
+docker exec -it solr bash -c "/bin/mv /opt/solr/server/solr/${SOLR_CORE_NAME} /opt/solr/server/solr/${PROJECT}"
+
