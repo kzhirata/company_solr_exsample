c26f8a21699f884d1be78d8dd9de1ca37c6d125b
kzhirata <kzhirata77@gmail.com>
2018-07-21 14:53:11 +0900

    fix

--- data/import.bash
+++ data/import.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+SOLR_CORE_NAME="company"
+
 for file in `ls [0-9]*.csv`; do
     echo "${file}"
     cat header.csv ${file} > csv.tmp
-    curl http://localhost:8983/solr/company/update?commit=true --data-binary @csv.tmp  -H 'Content-type:text/csv;charset=utf-8'
+    curl http://localhost:8983/solr/${SOLR_CORE_NAME}/update?commit=true --data-binary @csv.tmp  -H 'Content-type:text/csv;charset=utf-8'
 done
 
+rm csv.tmp 
