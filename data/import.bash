b632723606a88ffc57abb61d141d96ed68edbdd8
kzhirata <kzhirata77@gmail.com>
2018-07-21 14:48:46 +0900

    fix

--- data/import.bash
+++ data/import.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl http://localhost:8983/solr/coop/update?commit=true --data-binary @h_13_tokyo_all_20180629.csv  -H 'Content-type:text/csv;charset=utf-8'
+for file in `ls [0-9]*.csv`; do
+    echo "${file}"
+    cat header.csv ${file} > csv.tmp
+    curl http://localhost:8983/solr/company/update?commit=true --data-binary @csv.tmp  -H 'Content-type:text/csv;charset=utf-8'
+done
+
